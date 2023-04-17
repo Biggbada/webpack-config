fb7291e569dda893fbaf790374aa0fa94dc63e9d
Biggbada <yguenot@polepo.fr>
2023-04-17 14:49:30 +0200

    feat/webpack : Configuration babe et htmal-webpack-plugin
    Installation de babel loader et html-webpack-plugin avec configuration de babel-loader dans la propriété rules et configuration de webpackHtmlPlugin dans la propriété plugins qui va générer un fichier index.html dans le fichier dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require ('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');  //ici on declare le plug-in que l'on a installé
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.bundle.js'
-    }
+    },
+    module: {
+    rules:[
+        {
+            test: /\.js$/,              //antislash devant point pour bien dire qu'on cherche un .
+            exclude: /node_modules/,
+            use:{
+                loader: "babel-loader",
+            }
+        }
+    ] 
+},
+plugins:[
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/index.html'),
+    })
+]
 }
